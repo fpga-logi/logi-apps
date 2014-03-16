e2ddef729bc5ac741f5b29b6ad07cb3eaf00c4e9
Jonathan Piat <piat.jonathan@gmail.com>
2014-03-17 00:09:07 +0100

    small modification to add instruction to install stratum

--- bitcoin_app/make_demo.sh
+++ bitcoin_app/make_demo.sh
@@ -41,10 +41,14 @@ cd ..
 echo "done" > .done
 echo "wait for the following script to end"
 echo "result should be : nonce :7a33330e "
-echo "1) Create an account on mining pool (tested on btcguild and bitlc)"
-echo "2) Configure a worker on the pool website"
-echo "3) Fill config.py with your worker configuration"
-echo "4) Launch python mark1_rpi_miner.py and wait to get rich ..."
+echo "1) Create an account on mining pool (tested on btcguild)"
+echo "2) Configure a worker on a pool website"
+echo "3) If your pool server uses stratum (btcguild uses stratum)"
+echo "	3-1) run the install_stratum_proxy script : ./install_stratum_proxy"
+echo "	3-2) launch the proxy with your pool informations : ./stratum_proxy/mining_proxy.py -o <pool-url> -p <port> -sp 3334 -gp 8332"
+echo "	3-3) Fill config.py with worker name and \"host\" = \"127.0.0.1\", \"port\"=8332 as server info"
+echo "4) Fill config.py with your worker configuration"
+echo "4) Launch python logipi_miner.py and wait to get rich ..."
 echo "INFO: the hashrate (~1.8MHash/s) is intentionnaly limited to prevent the FPGA from overheating."
 echo "Because of the low hashrate (compared to bigger FPGA or ASIC), expect to wait some time before getting a valid share ..."
 $PYTHON logipi.py
