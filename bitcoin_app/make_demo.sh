dc2ea84e070fba35d0260b9d378cc3e83cb01621
Jonathan Piat <piat.jonathan@gmail.com>
2014-04-11 23:34:26 +0200

    small modification to the naming of th ebit file

--- bitcoin_app/make_demo.sh
+++ bitcoin_app/make_demo.sh
@@ -1,43 +1,20 @@
 #!/bin/sh
 
-read -p "What is your board version (RA1, RA2, RA3)" BOARD_VERSION
+BOARD_VERSION=R1
+sudo logi_loader ./logibone_mining_${BOARD_VERSION}.bit
 
-sudo logi_loader ./logipi_mining_${BOARD_VERSION}.bit
-
-DISTRO="$(cat /etc/os-release | grep "ID_LIKE=.*" | sed "s,ID_LIKE=,,")"
-echo $DISTRO
 
 if [ -f .done ]
 then
-
-if [ "$DISTRO" = "arch" ]; then
-PYTHON=python2
-else
 PYTHON=python
-fi
-
-$PYTHON logipi.py
+$PYTHON logibone.py
 exit
 fi
 
 
-if [ "$DISTRO" = "debian" ]; then
 sudo apt-get update
 sudo apt-get install python-dev
-PYTHON=python
-elif [ "$DISTRO" = "arch" ]; then
-pacman -S --needed python2
-PYTHON=python2
-else
-echo "unknown distro, please manually install gcc, make, v4l-utils, libjpeg-turbo"
-PYTHON=python
-fi
 
-git clone https://github.com/lthiery/SPI-Py.git spi_lib
-cd spi_lib
-$PYTHON setup.py build
-sudo $PYTHON setup.py install
-cd ..
 echo "done" > .done
 echo "wait for the following script to end"
 echo "result should be : nonce :7a33330e "
@@ -51,4 +28,4 @@ echo "4) Fill config.py with your worker configuration"
 echo "4) Launch python logipi_miner.py and wait to get rich ..."
 echo "INFO: the hashrate (~1.8MHash/s) is intentionnaly limited to prevent the FPGA from overheating."
 echo "Because of the low hashrate (compared to bigger FPGA or ASIC), expect to wait some time before getting a valid share ..."
-$PYTHON logipi.py
+$PYTHON logibone.py
