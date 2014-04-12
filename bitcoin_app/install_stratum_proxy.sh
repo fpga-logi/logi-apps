92cb19cfc4fd7ea9de4e80ecbba9af825a14eab7
Jonathan Piat <piat.jonathan@gmail.com>
2014-04-12 23:24:49 +0200

    small typo ...

--- bitcoin_app/install_stratum_proxy.sh
+++ bitcoin_app/install_stratum_proxy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git clone git://github.com/slush0/stratum-mining-proxy.git stratum_proxy
-cd stratum-proxy
+cd stratum_proxy
 sudo apt-get install python-dev python-twisted
 sed -i "s/'twisted>=12.2.0',//" setup.py
 sudo python distribute_setup.py
