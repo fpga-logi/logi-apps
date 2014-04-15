58e8f05f5c0ef18ba1a35cc967b360645fe594f2
Jonathan Piat <piat.jonathan@gmail.com>
2014-04-15 11:12:52 +0200

    small changes to mirror the logipi app repository

--- wishbone/make_demo.sh
+++ wishbone/make_demo.sh
@@ -2,5 +2,10 @@
 
 logi_loader logibone_wishbone_r1.bit
 
-echo "Read through the README file to understand how to control the peripheral connected"
+cd sw
+make
+
+echo "Read through the sw/README file to understand how to control the peripheral connected"
 echo "on the wishbone bus."
+echo "Use the test_wishbone.py python file to understand how to use the logi python class to access"
+echo "the wishbone address space." 
