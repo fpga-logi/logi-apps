5d8677da45041593ade5de4668c11ec137165833
Michael Jones <mjones@valentfx.com>
2014-04-15 22:04:06 +0000

    fixed the reference to the readme location

--- wishbone/make_demo.sh
+++ wishbone/make_demo.sh
@@ -6,6 +6,6 @@ cd sw
 make
 
 echo "\nNotes*************************************************************************************"
-echo "* Read through the sw/README file to understand how to control the peripheral connected on the wishbone bus."
+echo "* Read through the README file to understand how to control the peripheral connected on the wishbone bus."
 echo "* Use the test_wishbone.py python file to understand how to use the logi python class to access the wishbone address space." 
 echo "********************************************************************************************\n"
