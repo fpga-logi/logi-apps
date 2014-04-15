0269083e9b6f670c75e178778368c2625a76210c
Michael Jones <mjones@valentfx.com>
2014-04-15 15:46:37 +0000

    made the text output in the make_demo script more readable.  Was hard to read before

--- wishbone/make_demo.sh
+++ wishbone/make_demo.sh
@@ -5,7 +5,7 @@ logi_loader logipi_wishbone.bit
 cd sw
 make
 
-echo "Read through the sw/README file to understand how to control the peripheral connected"
-echo "on the wishbone bus."
-echo "Use the test_wishbone.py python file to understand how to use the logi python class to access"
-echo "the wishbone address space." 
+echo "\nNotes*************************************************************************************"
+echo "* Read through the sw/README file to understand how to control the peripheral connected on the wishbone bus."
+echo "* Use the test_wishbone.py python file to understand how to use the logi python class to access the wishbone address space." 
+echo "********************************************************************************************\n"
