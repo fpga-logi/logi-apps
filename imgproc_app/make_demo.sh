84c848aa8f8d7cc23dec911f426635c219c25e67
Michael Jones <mjones@valentfx.com>
2014-04-15 22:14:10 +0000

    more formatting

--- imgproc_app/make_demo.sh
+++ imgproc_app/make_demo.sh
@@ -12,11 +12,11 @@ if [ -f .done ]
 then
 cd ../tools/logi-mjpg-streamer/
 echo "\nDEMO NOW RUNNING ***************************************************************************"
-echo "Demo will now start :"
-echo "Open a browser and connect to http://<your beaglebone ip address>:8080/stream.html"
-echo "Use switches on the logibone to switch between video source (normal, gaussian, sobel, harris)"
-echo "full documentation on this app: http://valentfx.com/wiki/index.php?title=LOGI_-_Image_Processing_-_Project"
-echo "Press ctrl-c to end demo"
+echo "* Demo will now start :"
+echo "* Open a browser and connect to http://<your beaglebone ip address>:8080/stream.html"
+echo "* Use switches on the logibone to switch between video source (normal, gaussian, sobel, harris)"
+echo "* full documentation on this app:\nhttp://valentfx.com/wiki/index.php?title=LOGI_-_Image_Processing_-_Project"
+echo "* Press ctrl-c to end demo"
 echo "END INSTRUCTIONS ***************************************************************************\n"
 
 ./launch_streamer.sh 0
@@ -42,11 +42,12 @@ make -j2
 touch ${CUR_DIR}/.done
 
 echo "\nDEMO NOW RUNNING ***************************************************************************"
-echo "Demo will now start :"
-echo "Open a browser and connect to http://<your beaglebone ip address>:8080/stream.html"
-echo "Use switches on the logibone to switch between video source (normal, gaussian, sobel, harris)"
-echo "full documentation on this app: http://valentfx.com/wiki/index.php?title=LOGI_-_Image_Processing_-_Project"
-echo "Press ctrl-c to end demo"
+echo "* Demo will now start :"
+echo "* Open a browser and connect to http://<your beaglebone ip address>:8080/stream.html"
+echo "* Use switches on the logibone to switch between video source (normal, gaussian, sobel, harris)"
+echo "* full documentation on this app:\nhttp://valentfx.com/wiki/index.php?title=LOGI_-_Image_Processing_-_Project"
+echo "* Press ctrl-c to end demo"
 echo "END INSTRUCTIONS ***************************************************************************\n"
+
 
 ./launch_streamer.sh 0
