0d77384770d50e25f426b98ce41f65968e98186e
Michael Jones <mjones@valentfx.com>
2014-04-15 20:42:21 +0000

    re-formatted the text output on the terminal output

--- wishbone/make_demo.sh
+++ wishbone/make_demo.sh
@@ -5,7 +5,10 @@ logi_loader logibone_wishbone_r1.bit
 cd sw
 make
 
-echo "Read through the sw/README file to understand how to control the peripheral connected"
-echo "on the wishbone bus."
-echo "Use the test_wishbone.py python file to understand how to use the logi python class to access"
-echo "the wishbone address space." 
+echo "\nNotes*************************************************************************************"
+echo "* Read through the sw/README file to understand how to control the peripheral connected on the wishbone bus."
+echo "* Use the test_wishbone.py python file to understand how to use the logi python class to access the wishbone address space." 
+echo "* 1) user can run the python wishbone with command 'sudo python2 test_wishbone.py' " 
+echo "* 2) user can run the c code wishbone fucntiona call by using the /sw ./write_wishbone and ./read_wishbone  - See the sw/README.TXT"
+echo "********************************************************************************************\n"
+
