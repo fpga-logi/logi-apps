df0f29e5329ebe9b017cfcc897dbfe8891d16c24
Michael Jones <mjones@valentfx.com>
2014-04-16 01:12:00 +0000

    adding readme

--- blink_led_app/make_demo.sh
+++ blink_led_app/make_demo.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 logi_loader ./logipi_blink.bit
-echo "Logipi leds should now be blinking !"
+echo "\n*Description ************************************************************"
+echo "* This is a "hello world" demo for the LOGI FPGA.  This is shows the functioning nuts and bolts of the Pi/Bone and FPGA"
+echo "\n* See the blink LED wiki page for full documentation: http://valentfx.com/wiki/index.php?title=LOGI_-_LED_Blink_-_Project"
+echo "\n*  Logipi leds should now be blinking !"
+echo "*************************************************************************"
+
+
