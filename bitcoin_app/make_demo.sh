5735cf7646adf67d32505d27abe79706e0608dc1
Michael Jones <mjones@valentfx.com>
2014-04-16 00:58:05 +0000

    finally happy with the terminal output ;)

--- bitcoin_app/make_demo.sh
+++ bitcoin_app/make_demo.sh
@@ -6,10 +6,13 @@ sudo logi_loader ./logibone_mining_${BOARD_VERSION}.bit
 PYTHON=python
 if [ -f .done ]
 then
+
 echo "\nINSTRUCTIONS *******************************************************************************"
-echo "* This is a demo of the FPGA running the bitcoin mining Algorithm.  The FPGA iteratively cycle through the algorithm looking for the solution (nonce).  Once the solution is found the FPGA will report the solution back to the Pi/"
-echo "* The full minig support for the minor is no longer supported as this is simply for demo purposes and there are much better / cost effective solution avialable now that have replaced FPGAs."
-echo "INFO: the hashrate (~1.8MHash/s) is intentionnaly limited to prevent the FPGA from overheating." echo "Because of the low hashrate (compared to bigger FPGA or ASIC),"
+echo "* This is a demo of the FPGA running the bitcoin mining Algorithm.  The FPGA iteratively cycle through the algorithm looking for the solution (nonce).  Once the solution is found the FPGA will report the solution back to the Pi/Bone and be displayed on the screen."
+echo "\n* The full minig support for the minor is no longer supported as this is simply for demo purposes and there are much better / cost effective solution avialable now that have replaced FPGAs."
+echo "\nINFO: the hashrate (~1.8MHash/s) is intentionnaly limited to prevent the FPGA from overheating." echo "Because of the low hashrate (compared to bigger FPGA or ASIC)"
+echo "******************************************************************************************"
+
 $PYTHON logibone.py
 exit
 fi
@@ -19,18 +22,9 @@ touch .done
 echo "wait for the following script to end"
 echo "result should be : nonce :7a33330e "
 
-#NO LONGER SUPPORTING FULL MINING.  HARD TO KEEP UP WITH API
-#echo "1) Create an account on mining pool (tested on btcguild)"
-#echo "2) Configure a worker on a pool website"
-#echo "3) If your pool server uses stratum (btcguild uses stratum)"
-#echo "	3-1) run the install_stratum_proxy script : ./install_stratum_proxy"
-#echo "	3-2) launch the proxy with your pool informations : ./stratum_proxy/mining_proxy.py -o <pool-url> -p <port> -sp 3334 -gp 8332"
-#echo "	3-3) Fill config.py with worker name and \"host\" = \"127.0.0.1\", \"port\"=8332 as server info"
-#echo "4) Fill config.py with your worker configuration"
-#echo "4) Launch python logipi_miner.py and wait to get rich ..."
-
-
 echo "\nINSTRUCTIONS *******************************************************************************"
 echo "* This is a demo of the FPGA running the bitcoin mining Algorithm.  The FPGA iteratively cycle through the algorithm looking for the solution (nonce).  Once the solution is found the FPGA will report the solution back to the Pi/Bone and be displayed on the screen."
-echo "* The full minig support for the minor is no longer supported as this is simply for demo purposes and there are much better / cost effective solution avialable now that have replaced FPGAs."
-echo "INFO: the hashrate (~1.8MHash/s) is intentionnaly limited to prevent the FPGA from overheating." echo "Because of the low hashrate (compared to bigger FPGA or ASIC), expect to wait some time before getting a valid share ..." $PYTHON logibone.py
+echo "\n* The full minig support for the minor is no longer supported as this is simply for demo purposes and there are much better / cost effective solution avialable now that have replaced FPGAs."
+echo "\nINFO: the hashrate (~1.8MHash/s) is intentionnaly limited to prevent the FPGA from overheating." echo "Because of the low hashrate (compared to bigger FPGA or ASIC)"
+echo "******************************************************************************************"
+
