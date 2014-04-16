cb8a34167f07e742516ef84fcd56650b86b2ea0d
Michael Jones <mjones@valentfx.com>
2014-04-16 00:42:11 +0000

    removing proxy files

--- bitcoin_app/install_stratum_proxy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git clone git://github.com/slush0/stratum-mining-proxy.git
-cd stratum-mining-proxy
-sudo apt-get install python-dev python-twisted
-sed -i "s/'twisted>=12.2.0',//" setup.py
-sudo python distribute_setup.py
-sudo python setup.py develop
