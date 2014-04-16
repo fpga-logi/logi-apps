d76d17ef41af37e163796dae6d32483083c7d4f1
Michael Jones <mjones@valentfx.com>
2014-04-16 00:24:00 +0000

    removed the proxy files for logibone

--- bitcoin_app/install_stratum_proxy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git clone git://github.com/slush0/stratum-mining-proxy.git stratum_proxy
-cd stratum_proxy
-sudo apt-get install python-dev python-twisted
-sed -i "s/'twisted>=12.2.0',//" setup.py
-sudo python distribute_setup.py
-sudo python setup.py develop
