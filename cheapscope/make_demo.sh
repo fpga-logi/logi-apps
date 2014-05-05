b13a7aa8d9596eae133578854c819074c2867028
Michael Jones <mjones@valentfx.com>
2014-05-05 21:50:58 +0000

    adding cheapscope logi-app

---
+++ cheapscope/make_demo.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+logi_loader  logipi_virtual_component.bit
+
+if [ -f .done ]
+then
+logi_loader  cheapscope_count_demo.bit
+cat README.TXT
+./cheapscope /dev/ttyAMA0
+exit
+fi
+
+#.done file has not been run -- Run the setup 
+echo ".................Installing libncurses ................"
+sudo apt-get install libncurses5-dev
+echo "...................building cheapscope ................"
+#gcc -o cheapscope cheapscope.c -lncurses #using the pre-compiled.
+
+
+echo ".............We must disable ttyAMA0 and restart.........."
+./rpi-serial-consol disable
+echo "YOU MUST RESTART THE RPI BEFORE RUNNING THIS DEMO (restart with ttyAMA0 disabled)"
+echo "done" > .done
+
+
+echo "* Re-run the make_demo script when the Rpi restarts......"
+echo "....................RESTARTING NOW ......................"
+shutdown -r now
