f0110b481ff92ecdad2a22e80831bbe9a69583e5
Michael Jones <mjones@valentfx.com>
2014-05-06 15:51:52 +0000

    update readme, make_demo

--- edu_demo/make_demo.sh
+++ edu_demo/make_demo.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 logi_loader ./logi_edu_demo.bit
-echo "\n*Description ************************************************************"
-echo "* This a demo that will show some of the fucntions of the EDU board including 1) VGA Pong game 2) VGA bars color and gray 3) PS/2 mouse 4) NES controllers 5) audio."
-echo "\n* See the blink LED wiki page for full documentation: http://valentfx.com/wiki/index.php?title=LOGI_-_EDU_Demo_-_Project"
-echo "\n* Use the PB, SW to change and show functionality.  Plug in VGA, PS/2 mouse to port1, NES controllers to port1 or 2, speakers or earphones to audio jack"
-echo "*************************************************************************"
-
+cat README.TXT
 
